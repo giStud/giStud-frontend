3e39f2342154e3e0a50b35200c5a33c8b1957ada
CamperGit <sashchenkodmitrii@mail.ru>
2021-08-12 19:35:39 +0300

    delete unused code

--- src/pages/ScheduleLoading.vue
+++ src/pages/ScheduleLoading.vue
@@ -1,15 +1,5 @@
 <template>
   <div class="q-pa-md">
-    <div class="q-gutter-md row items-start">
-      <input type="file" @change="setFiles" multiple="">
-    </div>
-    <div>
-      <q-btn color="primary" style="width: 200px" @click="loadSchedules">
-        <div class="ellipsis">
-          Загрузить
-        </div>
-      </q-btn>
-    </div>
     <div>
       <q-uploader
         field-name="files"
@@ -29,9 +19,6 @@ import authHeader from "../services/authHeader";
 
 export default {
   setup() {
-
-    console.log(authHeader())
-    [{key : Auth, value : token}]
     const headers = Object.entries(authHeader()).map(([key, value])=>{
       return {name : key, value}
     })
