ca74857f6ab9f541fcf07b589af5aca224a6b3f7
Lex <50260829+4gname@users.noreply.github.com>
2021-08-29 22:26:57 +0700

    upd

--- src/components/static/MainNewsOur.vue
+++ src/components/static/MainNewsOur.vue
@@ -1,10 +1,7 @@
 <template>
   <div id="news-by-our" class="q-pa-md">
     <q-carousel animated v-model="slide" arrows navigation infinite>
-      <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg"/>
-      <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg"/>
-      <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg"/>
-      <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg"/>
+      <q-carousel-slide :name="1" img-src="https://i.imgur.com/JZwkYX1.jpg"/>
     </q-carousel>
   </div>
 </template>
