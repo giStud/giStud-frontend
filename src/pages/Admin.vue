c122fdbfb1ed8f7ba888a3585509ee81efec9bba
CamperGit <sashchenkodmitrii@mail.ru>
2021-09-03 21:49:45 +0300

    create create news creating in admin page

--- src/pages/Admin.vue
+++ src/pages/Admin.vue
@@ -22,24 +22,50 @@
             <div class="q-pa-md">
               <q-table bordered flat square title="Багулины" :rows="univRequestsRows"
                        :columns="univRequestsColumns" row-key="requestId" selection="multiple" separator="cell"
-                       v-model:selected="selectedRows" wrap-cells
+                       v-model:selected="selectedUserMessagesRows" wrap-cells
               />
             </div>
             <div class="q-pa-md">
-              <q-btn color="primary" label="Удалить выбранные записи" @click="deleteSelectedRows"/>
+              <q-btn color="primary" label="Удалить выбранные записи" @click="deleteSelectedUserMessagesRows"/>
             </div>
           </q-tab-panel>
 
           <q-tab-panel name="news">
             <div class="text-h4 q-mb-md">Новости</div>
             <div class="q-pa-md">
-              <q-table bordered flat square title="Список новостей" :rows="newsRequestsRows"
-                       :columns="newsRequestsColumns" row-key="requestId" selection="multiple" separator="cell"
+              <q-table bordered flat square title="Список новостей" :rows="newsRows"
+                       :columns="newsColumns" row-key="newsId" selection="multiple" separator="cell"
                        v-model:selected="selectedNewsRows" wrap-cells
               />
             </div>
             <div class="q-pa-md">
-              <q-btn color="primary" label="Удалить выбранные новости" @click="deleteSelectedRowsNews"/>
+              <q-btn color="primary" no-caps label="Удалить выбранные новости" @click="deleteSelectedNewsRows"/>
+            </div>
+            <div class="q-pa-md">
+
+              <div class="text-h5 q-mb-md">Добавить новость</div>
+              <q-input square outlined filled v-model="newsTitle" label="Заголовок"/>
+              <q-input square outlined filled v-model="newsImgSrc" label="Ссылка на фотографию"/>
+              <div class="text-h6">Короткий текст</div>
+              <q-editor
+                v-model="newsShortText"
+                :dense="$q.screen.lt.md"
+                :toolbar="newsToolbar"
+                :fonts="newsEditorFonts"
+              />
+              <div class="text-h6">Полный текст</div>
+              <q-editor
+                v-model="newsText"
+                :dense="$q.screen.lt.md"
+                :toolbar="newsToolbar"
+                :fonts="newsEditorFonts"
+                style="height: 500px"
+              />
+            </div>
+            <div class="q-pa-md">
+              <q-btn color="primary" no-caps label="Добавить" @click="handleNewsCreating(newsTitle,newsImgSrc ,newsShortText, newsText)"/>
+            </div>
+            <div v-html="newsExample">
             </div>
           </q-tab-panel>
 
@@ -56,13 +82,26 @@ import {onMounted, ref} from "vue";
 import UserMessagesService from "../services/other/userMessagesService"
 import {getDateString} from "src/composables/schedule/ScheduleTable";
 import UtilsService from "../services/other/utilsService"
+import NewsService from  "../services/other/newsService"
+import {useQuasar} from "quasar";
 
-const newsRequestsColumns = [{
+const newsEditorFonts = {
+  arial: 'Arial',
+  arial_black: 'Arial Black',
+  comic_sans: 'Comic Sans MS',
+  courier_new: 'Courier New',
+  impact: 'Impact',
+  lucida_grande: 'Lucida Grande',
+  times_new_roman: 'Times New Roman',
+  verdana: 'Verdana'
+}
+
+const newsColumns = [{
   name: 'id',
   required: true,
   label: 'Id',
   align: 'center',
-  field: 'requestId',
+  field: 'newsId',
   headerStyle: 'width: 200px',
   sortable: true
 }, {
@@ -71,7 +110,7 @@ const newsRequestsColumns = [{
   label: 'Заголовок',
   align: 'center',
   headerStyle: 'width: 500px',
-  field: 'text',
+  field: 'title',
   sortable: true
 }, {
   name: 'shortContent',
@@ -79,7 +118,7 @@ const newsRequestsColumns = [{
   label: 'Краткий текст',
   align: 'center',
   headerStyle: 'width: 500px',
-  field: 'shortContent',
+  field: 'shortText',
   sortable: true
 }, {
   name: 'content',
@@ -87,7 +126,7 @@ const newsRequestsColumns = [{
   label: 'Основной текст',
   align: 'center',
   headerStyle: 'width: 500px',
-  field: 'content',
+  field: 'fullText',
   sortable: true
 }, {
   name: 'img',
@@ -95,7 +134,7 @@ const newsRequestsColumns = [{
   label: 'Ссылка на картинку',
   align: 'center',
   headerStyle: 'width: 500px',
-  field: 'img',
+  field: 'imgSrc',
   sortable: true
 },
 ]
@@ -149,41 +188,149 @@ const univRequestsColumns = [{
 export default {
   name: "Admin",
   setup() {
+    const $q = useQuasar();
     const univRequestsRows = ref([]);
-    const selectedRows = ref([])
-    const newsRequestsRows = ref([]);
-    const selectedNewsRows = ref([])
-    onMounted(async () => {
-      univRequestsRows.value = await UserMessagesService.getUserMessages();
-      console.log(univRequestsRows.value)
-    })
+    const selectedUserMessagesRows = ref([])
 
-    const deleteSelectedRows = async () => {
-      if (selectedRows.value.length > 0) {
-        for (let selected of selectedRows.value) {
+    const deleteSelectedUserMessagesRows = async () => {
+      if (selectedUserMessagesRows.value.length > 0) {
+        for (let selected of selectedUserMessagesRows.value) {
           await UserMessagesService.deleteUserMessageById(selected.requestId)
         }
         univRequestsRows.value = await UserMessagesService.getUserMessages();
       }
     }
 
-    const deleteSelectedNewsRows = async () => {
-      if (selectedNewsRows.value.length > 0) {
-        for (let selected of selectedNewsRows.value) {
-          await UserMessagesService.deleteUserMessageById(selected.requestId)
-        }
-        newsRequestsRows.value = await UserMessagesService.getUserMessages();
+    const newsRows = ref([]);
+    const selectedNewsRows = ref([])
+    const newsTitle = ref('');
+    const newsShortText = ref('');
+    const newsImgSrc = ref('');
+    const newsText = ref('');
+    const newsExample = ref('');
+
+    const handleNewsCreating = async (title, img, shortText, fullText) => {
+      try {
+        const {data} = await NewsService.saveNews(title, img, shortText, fullText)
+        $q.notify({
+          color: "green-4",
+          textColor: "white",
+          icon: "cloud_done",
+          message: "Новость успешно добавлена",
+        });
+        newsRows.value.push(data);
+      } catch (e) {
+        console.log(e)
       }
     }
 
+    const deleteSelectedNewsRows = async () => {
+      if (selectedNewsRows.value.length > 0) {
+        for (let selected of selectedNewsRows.value) {
+          await NewsService.deleteNewsEntityById(selected.newsId)
+        }
+        newsRows.value = await NewsService.getNews();
+      }
+    }
+
+    onMounted( async () => {
+      univRequestsRows.value = await UserMessagesService.getUserMessages();
+      newsRows.value = await NewsService.getNews();
+      newsExample.value = newsRows.value[1].shortText;
+    })
+
+      const newsToolbar = [
+      [
+        {
+          label: $q.lang.editor.align,
+          icon: $q.iconSet.editor.align,
+          fixedLabel: true,
+          list: 'only-icons',
+          options: ['left', 'center', 'right', 'justify']
+        },
+        {
+          label: $q.lang.editor.align,
+          icon: $q.iconSet.editor.align,
+          fixedLabel: true,
+          options: ['left', 'center', 'right', 'justify']
+        }
+      ],
+        ['bold', 'italic', 'strike', 'underline', 'subscript', 'superscript'],
+        ['token', 'hr', 'link', 'custom_btn'],
+        ['print', 'fullscreen'],
+        [
+          {
+            label: $q.lang.editor.formatting,
+            icon: $q.iconSet.editor.formatting,
+            list: 'no-icons',
+            options: [
+              'p',
+              'h1',
+              'h2',
+              'h3',
+              'h4',
+              'h5',
+              'h6',
+              'code'
+            ]
+          },
+          {
+            label: $q.lang.editor.fontSize,
+            icon: $q.iconSet.editor.fontSize,
+            fixedLabel: true,
+            fixedIcon: true,
+            list: 'no-icons',
+            options: [
+              'size-1',
+              'size-2',
+              'size-3',
+              'size-4',
+              'size-5',
+              'size-6',
+              'size-7'
+            ]
+          },
+          {
+            label: $q.lang.editor.defaultFont,
+            icon: $q.iconSet.editor.font,
+            fixedIcon: true,
+            list: 'no-icons',
+            options: [
+              'default_font',
+              'arial',
+              'arial_black',
+              'comic_sans',
+              'courier_new',
+              'impact',
+              'lucida_grande',
+              'times_new_roman',
+              'verdana'
+            ]
+          },
+          'removeFormat'
+        ],
+        ['quote', 'unordered', 'ordered', 'outdent', 'indent'],
+
+        ['undo', 'redo'],
+        ['viewsource']
+      ]
+
     return {
-      selectedRows,
+      selectedUserMessagesRows,
       univRequestsRows,
       univRequestsColumns,
       selectedNewsRows,
-      newsRequestsRows,
-      newsRequestsColumns,
-      deleteSelectedRows,
+      newsRows,
+      newsColumns,
+      newsTitle,
+      newsShortText,
+      newsImgSrc,
+      newsText,
+      newsEditorFonts,
+      newsToolbar,
+      newsExample,
+      deleteSelectedUserMessagesRows,
+      handleNewsCreating,
       deleteSelectedNewsRows,
       tab: ref('bugs'),
       splitterModel: ref(10),
---
+++ src/services/other/newsService.js
@@ -0,0 +1,37 @@
+import {api} from "boot/axios";
+import authHeader from "src/services/auth/authHeader";
+
+class NewsService {
+  async saveNews(title, img, shortText, fullText) {
+    try {
+      return await api.post("/news/", {
+        title,
+        shortText,
+        imgSrc : img,
+        text : fullText
+      }, {headers: authHeader()})
+    } catch (e) {
+      throw e
+    }
+  }
+
+  async getNews() {
+    try {
+      const {data} = await api.get("/news")
+      return data;
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  async deleteNewsEntityById(newsId) {
+    try {
+      const {data} = await api.delete("/news/" + newsId, {headers: authHeader()})
+      return data;
+    } catch (e) {
+      throw e;
+    }
+  }
+}
+
+export default new NewsService();
