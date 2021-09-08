8a2d597d30230cdca81f8ed1e8c1cfe2589cff61
4gname <domian844@gmail.com>
2021-09-09 02:09:07 +0700

    add nav menu

--- src/layouts/MobileMainLayout.vue
+++ src/layouts/MobileMainLayout.vue
@@ -6,19 +6,29 @@
     </q-page-container>
 
     <!--HEADER-->
+    <q-footer>
+        <mHeader/>
+    </q-footer>
   </q-layout>
 </template>
 
 <script>
 
 import {defineComponent, ref} from 'vue'
+import mHeader from '../pages/mobile/components/mHeader.vue'
 
 export default defineComponent({
   name: 'MobileMainLayout',
 
   components: {
+    mHeader,
   },
 })
 </script>
 <style>
+body {
+  background-color: #edeef0;
+  font-family: 'Play', sans-serif;
+  min-width: 280px;
+}
 </style>
---
+++ src/pages/mobile/components/mHeader.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-card square class="row bg-white q-ma-none q-pa-none justify-between" style="max-height: 50px ; text-align: center">
+    <q-btn :to="'/'" round no-caps class="q-ml-sm nav-btn" flat>
+      <q-icon class="nav-icon" name="home" />
+      <span>Главная</span>
+    </q-btn>
+    <q-btn :to="'/schedule'" round no-caps class="nav-btn" flat>
+      <q-icon class="nav-icon" name="calendar_view_month" />
+      <span>Расписание</span>
+    </q-btn>
+    <q-btn :to="'/news'" round no-caps class="nav-btn" flat>
+      <q-icon class="nav-icon" name="feed" />
+      <span>Новости</span>
+    </q-btn>
+    <q-btn :to="'/announcement'" round no-caps class="nav-btn" flat>
+      <q-icon class="nav-icon" name="campaign" />
+      <span>Объявления</span>
+    </q-btn>
+    <q-btn :to="'/profile'" round no-caps class="q-mr-sm nav-btn" flat>
+      <q-icon class="nav-icon" name="badge" />
+      <span>Профиль</span>
+    </q-btn>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: "mHeader"
+}
+</script>
+
+<style scoped>
+.nav-btn {
+  height: 50px;
+  width: 50px;
+  background-color: #fff;
+}
+.nav-icon {
+  color: gray;
+  margin-bottom: -10px;
+}
+.nav-btn span {
+  font-size: 9px;
+  color: gray;
+  margin-bottom: -2px;
+}
+</style>
